6e93069b65a8aa688a8403f7b65197e9b2c6196e
Jason Ball <jasonlukeball@me.com>
2016-07-05 22:55:38 +1000

    Update db/deeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# ==========
+# users
+# ==========
+
+jason = User.create!(name: "Jason Ball", email: "jasonlukeball@me.com", password: "password")
+
+5.times do
+  User.create!( name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password(8))
+end
+
+users = User.all
+
+
+# ==========
+# lists
+# ==========
+
+25.times do
+  List.create!( user: users.sample, name: Faker::Hipster.sentence )
+end
+
+lists = List.all
+
+
+# ==========
+# todos
+# ==========
+
+Todo.find_or_create_by(name: "Something to do")
+
+100.times do
+  Todo.create!(user: users.sample, list: lists.sample, name: Faker::Hipster.words(4).join(' ') )
+end
+
+todos = Todo.all
+
+
+puts
+puts "DB Seed Finished!"
+puts "--------------------"
+puts "#{User.count} users created"
+puts "#{List.count} lists created"
+puts "#{Todo.count} todos created"
