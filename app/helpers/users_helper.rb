e24a1904f2ac5ebdef81bffc87d67f8bdbfc1f18
Jason Ball <jasonlukeball@me.com>
2016-07-10 21:15:35 +1000

    Add user motivation

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,10 @@
 module UsersHelper
 
   def user_message(user)
-    user_first_name = user.name.split.first
+    user_first_name   = user.name.split.first
+    random_motivation = ["Just do it.", "Don't let your dreams be dreams.", "Yesterday you said tomorrow.", "Make your dreams come true.", "Wake up and work hard at it.", "Nothing is impossible.", "Yes you can.", "If you're tired of starting over, stop giving up."].sample
     output = "<h2 id='user-message'>"
-    output += "#{user_first_name}, you have #{pluralize(user.todos.count, 'todo', 'to-dos')}...<br>Don't let your dreams be dreams."
+    output += "#{user_first_name}, you have #{pluralize(user.todos.count, 'todo', 'to-dos')}...<br>#{random_motivation}"
     output += "</h2>"
     output.html_safe
   end
