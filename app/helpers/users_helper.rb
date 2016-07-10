c2474d38f1cca7ed0b6dd248a8bad3cb29836d49
Jason Ball <jasonlukeball@me.com>
2016-07-10 14:16:48 +1000

    Update User Message on todo create/delete & list delete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def user_message(user)
+    user_first_name = user.name.split.first
+    output = "<h2 id='user-message'>"
+    output += "#{user_first_name}, you have #{pluralize(user.todos.count, 'todo', 'to-dos')}...<br>Don't let your dreams be dreams."
+    output += "</h2>"
+    output.html_safe
+  end
+
 end
